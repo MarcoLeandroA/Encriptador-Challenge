af8b9fcda704aa681f970cb9cdbfc56f8a6f0db6
Antonio <marcoaranibar12@gmail.com>
2024-01-30 19:19:36 -0500

    second version of encrypt, adding copy to clipboard, decrypt, remove image

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const textoIngresado = document.getElementById('texto-ingresado');
+const textoResultado = document.getElementById('texto-resultado');
+const btnEncriptar = document.getElementById('btn-encriptar');
+const btnDesencriptar = document.getElementById('btn-desencriptar');
+const textoVacio = document.getElementById('sin-palabras');
+const btnCopy = document.getElementById('btn-copy');
+
+
+
 const reemplazoVocales = {
   'e': 'enter',
   'i': 'imes',
@@ -5,28 +14,48 @@ const reemplazoVocales = {
   'o': 'ober',
   'u': 'ufat'
 };
-
-const textoIngresado = document.getElementById('texto-ingresado');
-const textoResultado = document.getElementById('texto-resultado');
-const btnEncriptar = document.getElementById('btn-encriptar');
-const btnDesencriptar = document.getElementById('btn-desencriptar');
-const textoVacio = document.getElementById('sin-palabras');
-
-let textoUsuario = textoIngresado.value;
-console.log(textoUsuario);
-
-btnEncriptar.addEventListener('click', () => {
-  reemplazarVocales(textoUsuario);
-})
-
-btnDesencriptar.addEventListener('click', () => {
-  textoResultado.textContent = textoUsuario;
-})
-
-function reemplazarVocales(palabra) {
-  const palabraModificada = palabra.replace(/[aeiou]/g, vocal => reemplazoVocales[vocal.toLowerCase()] || vocal);
-  textoResultado.textContent = palabraModificada;
+function quitarImagen() {
+  if (textoResultado.textContent.length > 0) {
+    textoVacio.style.display = 'none';
+  } else {
+    textoVacio.style.display = 'block';
+  }
+}
+function encriptarTexto() {
+  let textoUsuario = textoIngresado.value;
+  const textoModificado = textoUsuario.replace(/[aeiou]/g, vocal => reemplazoVocales[vocal.toLowerCase()] || vocal);
+  textoResultado.textContent = textoModificado;
 }
 
+btnEncriptar.addEventListener('click', () => {
+  encriptarTexto();
+  quitarImagen();
+});
+
+const reemplazoClaves = {
+  'enter': 'e',
+  'imes': 'i',
+  'ai': 'a',
+  'ober': 'o',
+  'ufat': 'u'
+};
+
+function desencriptarTexto() {
+  let textoUsuario = textoIngresado.value;
+  const textoOriginal = textoUsuario.replace(/enter|imes|ai|ober|ufat/g, clave => reemplazoClaves[clave]);
+  textoResultado.textContent = textoOriginal;
+}
+
+btnDesencriptar.addEventListener('click', () => {
+  desencriptarTexto();
+  quitarImagen();
+})
+
+function copyToClipboard(texto) {
+  return navigator.clipboard.writeText(texto);
+}
+btnCopy.addEventListener('click', () => {
+  copyToClipboard(textoResultado.textContent);
+});
 
 
